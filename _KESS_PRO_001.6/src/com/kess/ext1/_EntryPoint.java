a1b549735b896d9b016a3cd11945c08f7cdfa5b4
Janaki <ajrkess@gmail.com>
2018-11-27 19:39:53 +0530

    First Commit!

---
+++ _KESS_PRO_001.6/src/com/kess/ext1/_EntryPoint.java
@@ -0,0 +1,10 @@
+package com.kess.ext1;
+
+public class _EntryPoint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to KESS!! What is wrong with this.");
+	}
+
+}
